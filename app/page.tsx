47a2155f5834b899947be8a9cebca16614f1373d
Abdelrahman Salah <73993771+bebo698@users.noreply.github.com>
2026-07-20 16:16:28 +0300

    in

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function ComplaintForm() {
     try {
       // إضافة معرفات الواتساب مباشرة إلى البيانات المرسلة بدلاً من الرابط
       // هذا هو الأسلوب الصحيح والأكثر أماناً
-      params.append('instance_id', '6A4F8091C1D77');
+      params.append('instance_id', '6A5DEDDCA1DEA');
       params.append('access_token', '6a4ec34462a98');
 
       params.append('name', formData.get('name') as string);
